4d57ca737448581a2ccd00b0d27e381172a6728c
Hendre <hendre@mn8.ee>
2020-12-29 15:45:08 +0200

    added flags for runtime

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+
 	"github.com/RonnanSouza/kafka_environment/consumer"
 	"github.com/RonnanSouza/kafka_environment/producer"
-	"os"
 )
 
-func main()  {
-	if os.Args[1] == "consumer" {
-		consumer.StartConsumer()
-	} else if os.Args[1] == "producer" {
+func main() {
+
+	// Flags
+	consumerFlagValue := flag.Bool("c", false, "Use this flag to start a Kafka Consumer")
+	producerFlagValue := flag.Bool("p", false, "Use this flag to start a Kafka Producer")
+
+	// Flag Processing
+	flag.Parse()
+
+	// Decision Time
+	if *producerFlagValue == true {
 		producer.StartProducer()
+	} else if *consumerFlagValue == true {
+		consumer.StartConsumer()
+	} else {
+		fmt.Print("Usage: \n -c     Use this flag to start a Kafka Consumer\n -p     Use this flag to start a Kafka Producer\n")
 	}
 }
